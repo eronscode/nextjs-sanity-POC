a9c88ec54b13cb389f838f44228b65cf780b3dd1
Ose Matthew <eronscode@gmail.com>
2023-02-17 01:50:50 +0100

    feat(backend): modifies import of schema

--- backend/schemas/index.ts
+++ backend/schemas/index.ts
@@ -1,22 +1,28 @@
-import blockContent from './blockContent'
-import crewMember from './crewMember'
-import castMember from './castMember'
-import movie from './movie'
-import person from './person'
-import screening from './screening'
-import plotSummary from './plotSummary'
-import plotSummaries from './plotSummaries'
+import pages from './documents/pages'
+import siteSettings from './documents/settings'
+import navigation from './documents/navigation'
+
+// objects
+import hero from './objects/hero'
+import callToAction from './objects/callToAction'
+import seo from './objects/seo'
+import link from './objects/link'
+import navItem from './objects/navItem'
+import footer from './objects/footer'
+import portableText from './objects/portableText'
 
 export const schemaTypes = [
   // Document types
-  movie,
-  person,
-  screening,
+  pages,
+  navigation,
+  siteSettings,
 
   // Other types
-  blockContent,
-  plotSummary,
-  plotSummaries,
-  castMember,
-  crewMember,
+  hero,
+  callToAction,
+  seo,
+  link,
+  navItem,
+  footer,
+  portableText,
 ]
