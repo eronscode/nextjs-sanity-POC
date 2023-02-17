3f9a8d5a59b0c0b37c94a49c13b02af4f5104135
Ose Matthew <eronscode@gmail.com>
2023-02-17 01:54:18 +0100

    feat(frontend): creates utility file to define sanity api funtions

--- frontend/lib/sanity.client.ts
+++ frontend/lib/sanity.client.ts
@@ -1,7 +1,41 @@
-import {createClient} from 'next-sanity'
+import { PagePayload } from "./../types/pages";
+import { createClient } from "next-sanity";
+import { homePageQuery, pagePathsQuery, pagesBySlugQuery, settingsQuery } from "./sanity.queries";
+import { SettingsPayload } from "@/types/global";
 
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET // "production"
-export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION // "2023-01-01"
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET; // "production"
+export const apiVersion = process.env.NEXT_PUBLIC_SANITY_API_VERSION; // "2023-01-01"
 
-export const client = createClient({projectId, dataset, apiVersion, useCdn: true})
+export const sanityClient = (token?: string) =>
+  createClient({ projectId, dataset, apiVersion, useCdn: true, token });
+
+export async function getHomePage({
+  token,
+}: {
+  token?: string;
+}): Promise<{ frontPage: PagePayload } | undefined> {
+  return await sanityClient(token)?.fetch(homePageQuery);
+}
+
+export async function getSettings({
+  token,
+}: {
+  token?: string;
+}): Promise<SettingsPayload | undefined> {
+  return await sanityClient(token)?.fetch(settingsQuery);
+}
+
+export async function getPageBySlug({
+  slug,
+  token,
+}: {
+  slug: string
+  token?: string
+}): Promise<PagePayload | undefined> {
+  return await sanityClient(token)?.fetch(pagesBySlugQuery, { slug })
+}
+
+export async function getPagePaths(): Promise<string[]> {
+  return await sanityClient()?.fetch(pagePathsQuery)
+}
