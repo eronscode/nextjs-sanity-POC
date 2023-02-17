71097ef631293165001a88998c0dd20984d07ae7
Ose Matthew <eronscode@gmail.com>
2023-02-17 01:48:35 +0100

    feat(backend): modifies page reference in portableText

--- backend/schemas/objects/portableText.ts
+++ backend/schemas/objects/portableText.ts
@@ -48,10 +48,7 @@ export default {
                 title: 'Reference',
                 to: [
                   {
-                    type: 'home',
-                  },
-                  {
-                    type: 'cmsPage',
+                    type: 'page',
                   },
                 ],
               },
