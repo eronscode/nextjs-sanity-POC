08177e9edba0d9832a164a0a2c0ae2d344440b2c
Ose Matthew <eronscode@gmail.com>
2023-02-22 06:23:42 +0100

    feat(backend): imports defined schema

--- backend/schemas/index.ts
+++ backend/schemas/index.ts
@@ -1,6 +1,14 @@
 import pages from './documents/pages'
 import siteSettings from './documents/settings'
 import navigation from './documents/navigation'
+import authors from './documents/blog/authors'
+import posts from './documents/blog/posts'
+import service from './documents/service'
+import blogPage from './documents/blogPage'
+
+import customerReviews from './modules/customerReviews'
+import pricings from './modules/pricings'
+import services from './modules/services'
 
 // objects
 import hero from './objects/hero'
@@ -10,12 +18,25 @@ import link from './objects/link'
 import navItem from './objects/navItem'
 import footer from './objects/footer'
 import portableText from './objects/portableText'
+import customerReview from './documents/customerReview'
+import pricing from './documents/pricing'
+import textWithImage from './objects/textWithImage'
+import genericText from './objects/genericText'
 
 export const schemaTypes = [
   // Document types
   pages,
   navigation,
   siteSettings,
+  posts,
+  authors,
+  service,
+  blogPage,
+
+  //modules
+  customerReviews,
+  pricings,
+  services,
 
   // Other types
   hero,
@@ -25,4 +46,8 @@ export const schemaTypes = [
   navItem,
   footer,
   portableText,
+  customerReview,
+  pricing,
+  textWithImage,
+  genericText,
 ]
