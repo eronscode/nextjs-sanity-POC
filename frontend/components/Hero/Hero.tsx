c6370bf5b3107b655cea9e77868ff027cd213ea6
Ose Matthew <eronscode@gmail.com>
2023-02-22 06:48:27 +0100

    feat(frontend): adds padding and button link to Hero component

--- frontend/components/Hero/Hero.tsx
+++ frontend/components/Hero/Hero.tsx
@@ -2,22 +2,26 @@ import Link from "next/link";
 import type { Image } from "sanity";
 import React, { FunctionComponent } from "react";
 import { urlForImage } from "@/lib/sanity.image";
+import { LinkComponent } from "../Link/Link";
 
 export type HeroProps = SanityDocument & {
   heading?: string;
   tagline?: string;
   image?: Image;
+  button?: LinkProps;
 };
 
 export const Hero: FunctionComponent<HeroProps> = ({
   heading,
   tagline,
   image,
+  button,
 }) => {
   const imageUrl = image && urlForImage(image)?.url();
+  
   return (
     <section
-      className="py-40 w-full table relative  bg-top bg-cover bg-no-repeat"
+      className="py-40 w-full table relative  bg-center bg-cover bg-no-repeat"
       style={{ backgroundImage: `url(${imageUrl})` }}
     >
       <div className="absolute inset-0 bg-gradient-to-t to-black/50 via-black/75 from-black"></div>
@@ -35,12 +39,13 @@ export const Hero: FunctionComponent<HeroProps> = ({
           )}
 
           <div className="mt-8">
-            <a
-              href="#"
-              className="btn bg-violet-600 hover:bg-violet-700 border-violet-600 hover:border-violet-700 text-white rounded-md"
-            >
-              <i className="uil uil-envelope"></i> Contact Now
-            </a>
+            {button?.linkText && (
+              <LinkComponent
+                url={button?.url}
+                linkText={button?.linkText}
+                className="btn bg-violet-600 hover:bg-violet-700 border-violet-600 hover:border-violet-700 text-white rounded-md"
+              />
+            )}
           </div>
         </div>
       </div>
