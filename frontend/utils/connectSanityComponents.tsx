45845b3c0ce6f2b613ea704d9945a46fc076a00e
Ose Matthew <eronscode@gmail.com>
2023-02-22 06:31:53 +0100

    feat(frontend): adds more components to components map declaration

--- frontend/utils/connectSanityComponents.tsx
+++ frontend/utils/connectSanityComponents.tsx
@@ -1,18 +1,18 @@
 import React from "react";
 
-import { Hero, HeroProps } from "@/components/Hero/Hero";
+import { Hero } from "@/components/Hero/Hero";
 import { PagePayload } from "@/types/pages";
-
-const ComponentNotFound = ({ component }: { component: string }) => (
-  <h3>
-    The component <strong style={{ color: "red" }}>{component}</strong> has not
-    been created yet.
-  </h3>
-);
+import { TextWithImage } from "@/components/TextWithImage/TextWithImage";
+import { ServicesGrid } from "@/components/ServicesGrid/ServicesGrid";
+import { Pricing } from "@/components/Pricing/Pricing";
+import { Reviews } from "@/components/Reviews/Reviews";
+import { GenericText } from "@/components/GenericText/GenericText";
 
 interface Props {
   sections: PagePayload["pageSections"];
 }
+
+
 export const PageComponentMap: React.FunctionComponent<Props> = ({
   sections,
 }) => {
@@ -22,10 +22,28 @@ export const PageComponentMap: React.FunctionComponent<Props> = ({
         switch (section._type) {
           case "section_hero":
             return <Hero key={key} {...section} />;
+          case "section_textWithImage":
+            return <TextWithImage key={key} {...section} />;
+          case "section_services":
+            return <ServicesGrid key={key} {...section} />;
+          case "section_pricings":
+            return <Pricing key={key} {...section} />;
+          case "section_customerReviews":
+            return <Reviews key={key} {...section} />;
+          case "section_genericText":
+            return <GenericText key={key} {...section} />;
           default:
-            return <ComponentNotFound component={section._type} />;
+            return <ComponentNotFound key={key} component={section._type} />;
         }
       })}
     </>
   );
 };
+
+
+const ComponentNotFound = ({ component }: { component: string }) => (
+  <h3>
+    The component <strong style={{ color: "red" }}>{component}</strong> has not
+    been created yet.
+  </h3>
+);
